06bd859372620d01ddc540b9fa1036dd02e4217e
TurisMendes <turismendes.projeto@gmail.com>
2025-04-15 04:04:14 +0000

    feat: insert id property and remove linkUrl from cards

--- public/typesHomePage.ts
+++ public/typesHomePage.ts
@@ -2,36 +2,36 @@ import { ImageData } from "./generalTypes";
 
 export interface HomeBanner {
   imageData: ImageData;
-  linkUrl: string;
   description?: string;
+  linkUrl: string;
 }
 
 export interface AtracaoLocalHomeCard {
+  id: string;
   title: string;
   shortDescription: string;
-  linkUrl: string;
   imageData: ImageData;
 }
 
 export interface AgenteCulturalHomeCard {
+  id: string;
   name: string;
   shortDescription: string;
-  linkUrl: string;
   imageData: ImageData;
 }
 
 export interface TrilhasHomeCard {
+  id: string;
   title: string;
   level: string;
   shortDescription: string;
-  linkUrl: string;
   imageData: ImageData;
 }
 
 export interface EventHomeCard {
+  id: string;
   eventTitle: string;
   shortDescription: string;
-  linkUrl: string;
   imageData: ImageData;
 }
 
