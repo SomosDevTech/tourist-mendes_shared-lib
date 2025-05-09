37dab29d81e4717979bd0be14ba8d9d4208e051d
mfcastilho <fredericocastilho@hotmail.com>
2025-05-08 22:35:36 -0300

    feat: create EditUsuarioLoggedSuperAdminType and EditUsarioLoggedAdminType

--- cms/FullUsuarioCmsType.ts
+++ cms/FullUsuarioCmsType.ts
@@ -3,6 +3,7 @@ import { Role } from "@/config/prisma/generatedFiles";
 export interface FullUsuarioCmsType {
   id: string;
   username: string;
+  usernameSort: string;
   email: string;
   role: Role
 }
@@ -11,4 +12,16 @@ export interface UsuarioCmsFiltersType {
   username: string;
   status: boolean
   createdDate: Date
+}
+
+export interface EditUsuarioLoggedSuperAdminType {
+  username?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+}
+
+export interface EditUsarioLoggedAdminType {
+  password: string;
+  confirmPassword: string;
 }
