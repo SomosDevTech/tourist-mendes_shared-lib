419f3a4c3d2b8409324085131b351d354cb7b1eb
mfcastilho <fredericocastilho@hotmail.com>
2025-05-19 14:40:49 -0300

    feat: create public/FeatureCollection.ts and public/MapaInterativoAtracaoLocalPointsType.ts

---
+++ public/MapaInterativoAtracaoLocalPointsType.ts
@@ -0,0 +1,9 @@
+export interface MapaInterativoAtracaoLocalPointsType {
+  id: string;
+  name: string;
+  shortdescription: string;
+  addressPoint: {
+    latitude: number;
+    longitude: number;
+  }
+}
